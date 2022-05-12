74f9ba12349e1ea1abf58a64ad7805b8280a0972
mddeepu8 <84235132+mddeepu8@users.noreply.github.com>
2022-05-12 17:38:00 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "dirac_arn" {
-  value       = github_branch_protection_v3.main_protection[0].arn
+  value       = github_branch_protection_v3.main_protection[0].repository
   description = "The ARN for user Paul Dirac"
 }
 
 output "all_arns" {
-  value       = github_branch_protection_v3.main_protection[*].arn
+  value       = github_branch_protection_v3.main_protection[*].repository
   description = "The ARNs for all users"
 }
