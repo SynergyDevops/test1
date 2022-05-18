ada0c11beed1be65241dfe780db608bd961d23a1
mddeepu8 <84235132+mddeepu8@users.noreply.github.com>
2022-05-18 11:34:13 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
-output "dirac_arn" {
+output "current_repository" {
   value       = github_branch_protection_v3.main_protection[0].repository
-  description = "The ARN for user Paul Dirac"
+  description = "This is the current repository in which workflow is running"
 }
 
-output "all_arns" {
+output "all_repositories" {
   value       = github_branch_protection_v3.main_protection[*].repository
-  description = "The ARNs for all users"
+  description = "List of all repositories defined"
 }
