2ff68eb2feb06b42110503120b6e0dfd485c1756
mddeepu8 <84235132+mddeepu8@users.noreply.github.com>
2022-11-29 14:15:56 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,8 +1,3 @@
-output "current_repository" {
-  value       = github_branch_protection_v3.main_protection[0].repository
-  description = "This is the current repository in which workflow is running"
-}
-
 output "all_repositories" {
   value       = github_branch_protection_v3.main_protection[*].repository
   description = "List of all repositories defined"
